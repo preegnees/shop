60662dc640879083dc884e6beed4ff7327825ea5
preegnees <mrraziapov13@gmail.com>
2022-05-27 02:21:45 +0300

    прописал класс для инициализации бота

---
+++ frontend/bot/utils/bot_init.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, types
+import aiogram
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from utils import config
+
+class Tengers():
+    _bot = None
+    _dp = None
+
+    def __init__(self) -> None:
+        self._bot = Bot(token=config.get_token())
+        self._dp = Dispatcher(self._bot)
+
+    def run(self) -> None:
+        executor.start_polling(dispatcher=self._dp, skip_updates=True)
+    
+    def get_bot(self) -> aiogram.Bot:
+        return self._bot
+
+    def get_dispatcher(self) -> aiogram.Dispatcher:
+        return self._dp
+
+tengers = Tengers()
