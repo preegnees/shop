34be0456a50512e5c7501018bd8f149e1e5410d3
preegnees <mrraziapov13@gmail.com>
2022-05-28 00:52:45 +0300

    добавил функцию для удаления pycache

--- frontend/bot/bot.py
+++ frontend/bot/bot.py
@@ -5,10 +5,22 @@ from utils import tengers
 from handlers import echo
 
 def start():
-    print("start")
     tengers.run()
 
+def _remove_pycache(path):
+    import os
+    import shutil
+    dir_list = os.listdir(path)
+    for i in dir_list:
+        if i == "__pycache__":
+            shutil.rmtree(path + "\\" + i)
+        else:
+            if os.path.isdir(path + "\\" + i):
+                _remove_pycache(path + "\\" + i)
+
+
 if __name__ == "__main__":
+    _remove_pycache("C:\\Users\\79174\\Desktop\\shop\\course_rest_api\\frontend\\bot")
     logger.info("Bot is starting")
     start()
     logger.info("Bot is stopped")
