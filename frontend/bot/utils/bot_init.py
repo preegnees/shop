8202843eef0628d0ebbe0510cc3ed16f0f824999
preegnees <mrraziapov13@gmail.com>
2022-05-28 00:52:01 +0300

    убрал взамодействи с типами через в этом файле

--- frontend/bot/utils/bot_init.py
+++ frontend/bot/utils/bot_init.py
@@ -1,18 +1,17 @@
-from aiogram import Bot, types
+from aiogram import Bot
 import aiogram
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from .config import config
 
+
 class Tengers():
     _bot = None
     _dp = None
-    _types = None
 
     def __init__(self) -> None:
         self._bot = Bot(token=config.get_token())
         self._dp = Dispatcher(self._bot)
-        self._types = types
 
     def run(self) -> None:
         executor.start_polling(dispatcher=self._dp, skip_updates=True)
@@ -22,9 +21,6 @@ class Tengers():
 
     def get_dispatcher(self) -> aiogram.Dispatcher:
         return self._dp
-
-    def get_types(self) -> aiogram.types:
-        return self._types
 
 
 tengers = Tengers()
