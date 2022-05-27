e57f75bf59b05b6d9cf5c9364744c5b978566c6d
preegnees <mrraziapov13@gmail.com>
2022-05-28 00:53:23 +0300

    экспериментирую с хендлерами

--- frontend/bot/handlers/echo.py
+++ frontend/bot/handlers/echo.py
@@ -1,9 +1,27 @@
 from utils import tengers
+from buttons import types, kb_cli
 
 class Echo:
-    dp = tengers.get_dispatcher()
-    @dp.message_handler()
-    async def echo_send(message: tengers.get_types().Message):
-        await message.answer(message.text)
+    dp = tengers.get_dispatcher() 
+    bot = tengers.get_bot()
 
+    @dp.message_handler(commands=['start'])
+    async def start(message: types.Message):
+        await message.reply(text="hello", reply_markup=kb_cli)
+    
+    @dp.message_handler()
+    async def other(message: types.Message):
+        print(message.contact.phone_number)
+
+        try:
+            msg = message.reply_to_message.text # if replied
+        except AttributeError:
+            msg = 'not replied'
+        
+        print(msg)
+
+        # не знаю как получить reply на мое сообщение
+
+        await message.reply(message.contact.phone_number)
+    
 echo = Echo()
