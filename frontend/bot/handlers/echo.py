3bd77866c20a0841a98952110165aa7264559a89
preegnees <mrraziapov13@gmail.com>
2022-05-27 21:06:28 +0300

    оставил только отпарвку сообщения, а ответ убрал

--- frontend/bot/handlers/echo.py
+++ frontend/bot/handlers/echo.py
@@ -5,7 +5,5 @@ class Echo:
     @dp.message_handler()
     async def echo_send(message: tengers.get_types().Message):
         await message.answer(message.text)
-        # await message.reply(message.text)
-        # await message.send_message(message.from_user.id, message.text)
 
 echo = Echo()
